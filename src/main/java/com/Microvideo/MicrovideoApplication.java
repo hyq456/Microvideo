ae4954b46dc4687c510e73f138d7280188736808
hyq <544608734@qq.com>
2019-07-02 14:18:20 +0800

    Initial commit

---
+++ src/main/java/com/Microvideo/MicrovideoApplication.java
@@ -0,0 +1,13 @@
+package com.Microvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrovideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrovideoApplication.class, args);
+	}
+
+}
